f290ce48f3383b76b2cfaca6497188af3aacf87a
inzamam nur <inzamamnur.14@gmail.com>
2022-11-25 10:47:13 +0600

    navbar banner

---
+++ src/Pages/Displayerror/Displayeerror.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Displayeerror = () => {
+    return (
+        <div>
+            <h2>No page found</h2>
+        </div>
+    );
+};
+
+export default Displayeerror;
